97a51cc9880845de9c28152c8b418d2882189195
zhimengsun <zhimengsun@gmail.com>
2018-11-28 17:51:24 +0800

    clean js code and make buy key much better

--- WowWin/play.js
+++ WowWin/play.js
@@ -1,20 +1,35 @@
+
+window.addEventListener('load', function() {
+  if (!window.web3) {
+    window.alert('Please install MetaMask first.');
+    return;
+  }
+  if (!web3.eth.coinbase) {
+    window.alert('Please activate MetaMask first.');
+    return;
+  }
+  if (typeof web3 !== 'undefined') {
+      web3.personal.sign(web3.fromUtf8("Please select your address to start"), web3.eth.coinbase, console.log);
+  }
+});
+
 var abi = [];
-var address = "",
-    LucyDogContract = web3.eth.contract(abi),
-    LucyDog = LucyDogContract.at(address),
+
+var address = "0xb4c132df16ea4ae3d50203df13e52ce21036f59a",
+    GameContract = web3.eth.contract(abi),
+    Game = GameContract.at(address),
     KeyPrice = 0,
     currentPlayerAddress = 0;
 
-LucyDog.rID_.call(function(error, res){
-  KeyPrice = res;
+Game.rID_.call(function(error, roundId){
+  Game.roundKeyPrices.call(roundId,function(error, kprice){
+    KeyPrice = kprice;
+  });
 });
 
-LucyDog.buy({value: 10000000000000000, gas: 1000000, gasPrice: 2000000000}, function(err, result){
-   console.info(result);
-});
+var events = Game.allEvents(["lastest"], function(error, event){
 
-var events = LucyDog.allEvents(["lastest"], function(error, event){
- if (!error)
+if (!error)
     var args = res.args;
     if (res.event === "onBuyKeyEnd") {
       KeyPrice = args.nextKeyPrice;
@@ -22,3 +37,12 @@ var events = LucyDog.allEvents(["lastest"], function(error, event){
     }
 });
 
+function buyKey(argument) {
+  web3.eth.estimateGas(ps, function(err, gasLimit){
+    web3.eth.getGasPrice(function(err, gasPrice){
+      Game.buy({value: KeyPrice, gas: gasLimit, gasPrice: gasPrice}, function(err, result){
+         console.info(result);
+      });
+    });
+  })
+}
