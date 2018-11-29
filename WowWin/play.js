cd1ce7bc32ec1046cdc3467ce5d5db2b549a041e
zhimengsun <zhimengsun@gmail.com>
2018-11-29 11:04:56 +0800

    minor clean

--- WowWin/play.js
+++ WowWin/play.js
@@ -19,6 +19,8 @@ var address = "0xb4c132df16ea4ae3d50203df13e52ce21036f59a",
     GameContract = web3.eth.contract(abi),
     Game = GameContract.at(address),
     KeyPrice = 0,
+    defaultBuyGas = 100000,
+    defaultGasPrice = 3000000000,
     currentPlayerAddress = 0;
 
 Game.rID_.call(function(error, roundId){
@@ -37,9 +39,17 @@ if (!error)
     }
 });
 
-function buyKey(argument) {
-  web3.eth.estimateGas(ps, function(err, gasLimit){
+var buyParams  = {
+  from: web3.eth.coinbase, 
+  to: address, 
+  data: "0xa6f2ae3a", 
+  value: KeyPrice
+}
+
+function buyKey() {
+  web3.eth.estimateGas(buyParams, function(err, gasLimit){
     web3.eth.getGasPrice(function(err, gasPrice){
+      console.info("GAS: " + gasLimit, "GASPRICE" + gasPrice);
       Game.buy({value: KeyPrice, gas: gasLimit, gasPrice: gasPrice}, function(err, result){
          console.info(result);
       });
